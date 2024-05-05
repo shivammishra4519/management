b8255eedd64d9287bc582471a8221528d0fe3a5e
shivammishra4519 <misra3408@gmail.com>
2024-05-05 18:57:36 +0530

    asand

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     // apiUrl: 'http://localhost:3000/',
     // apiUrl: 'https://umfmobileapi.store/',
-    apiUrl: 'http://62.72.56.135:3260/',
+    apiUrl: 'http://localhost:3260/',
     smsApiUrl:'https://textsms.vastbazaar.com/app/smsapi/index.php',
     key: '36138A813805F9',
     campaign:'13393',
